40a894d23c2a2bd4e1fd8859d5d7b184213e5827
EiriniChalatsiadou <canadian14@gmail.com>
2023-02-19 20:14:00 +0000

    function for increaseByOneResultBoard and added favicon

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,42 @@
 //This is the function for clearBoard
 function clearBoard() {
     let tiles = document.getElementsByClassName('tile');
-    for (let i=0; i<tiles.length; i++) {
+    for (let i = 0; i < tiles.length; i++) {
         tiles[i].classList.remove("player-O-icon");
         tiles[i].classList.remove("player-X-icon");
     }
 }
+const winnerX = "WINNER-X";
+const winnerO = "WINNER-O";
+const winnerDRAW = "WINNER-DRAW";
 
-// clearBoard();
 
+function increaseByOneResultBoard(result) {
+    if (result === winnerX) {
+        let winx = document.getElementById("win-X").innerHTML;
+        document.getElementById("win-X").innerHTML = Number(winx) + 1;
+    } else if (result === winnerO) {
+        let wino = document.getElementById("win-O").innerHTML;
+        document.getElementById("win-O").innerHTML = Number(wino) + 1;
+    } else if (result === winnerDRAW) {
+        let draw = document.getElementById("draw").innerHTML;
+        document.getElementById("draw").innerHTML = Number(draw) + 1;
+    }
+}
+
+// increaseByOneResultBoard(winnerX);
+// increaseByOneResultBoard(winnerO);
+// increaseByOneResultBoard(winnerDRAW);
+
+
+
+//addX(position)
+//addO(position)
+
+//calculateMove
 //startGame()
 
 //decideWinner()
-
-//setWinsX
-
-//setWinsO
-
-//setDraw
-
-//calculateMove
+//thereIsAWinner
+//blinkWinningRow
+//makeWinningSound
